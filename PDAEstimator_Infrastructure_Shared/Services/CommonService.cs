168c2e654360887982c61c4736f2df293b6fb4a1
alpeshpatel87 <50635245+alpeshpatel87@users.noreply.github.com>
2024-06-12 18:46:27 +0530

    Login OTP

---
+++ PDAEstimator_Infrastructure_Shared/Services/CommonService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDAEstimator_Infrastructure_Shared.Services
+{
+    public class CommonService
+    {
+    }
+
+    public static class GenerateOTP
+    {
+        private static readonly ThreadLocal<System.Security.Cryptography.RandomNumberGenerator> crng = new ThreadLocal<System.Security.Cryptography.RandomNumberGenerator>(System.Security.Cryptography.RandomNumberGenerator.Create);
+        private static readonly ThreadLocal<byte[]> bytes = new ThreadLocal<byte[]>(() => new byte[sizeof(int)]);
+        public static int NextInt()
+        {
+            crng.Value.GetBytes(bytes.Value);
+            return BitConverter.ToInt32(bytes.Value, 0) & int.MaxValue;
+        }
+        public static double NextDouble()
+        {
+            while (true)
+            {
+                long x = NextInt() & 0x001FFFFF;
+                x <<= 31;
+                x |= (long)NextInt();
+                double n = x;
+                const double d = 1L << 52;
+                double q = n / d;
+                if (q != 1.0)
+                    return q;
+            }
+        }
+    }
+}
